fb24fb0d2023e9bc0f413709970c52e84170b4a1
hoanglinh1119 <57972639+hoanglinh1119@users.noreply.github.com>
2020-03-21 22:53:06 +0700

    add new project

---
+++ time-zone/src/main/java/controller/TimeZoneController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+@Controller
+
+public class TimeZoneController {
+
+    @GetMapping ("/worldClock")
+    public String getTimeZone(ModelMap modelMap, @RequestParam (name="city", required = false,defaultValue = "Asia/Ho_Chi_Minh")String city)
+    {
+        Date date=new Date();
+        TimeZone local=TimeZone.getDefault();
+        TimeZone locale= TimeZone.getTimeZone("city");
+        long locale_time=date.getTime()+(locale.getRawOffset()-local.getRawOffset());
+        date.setTime(locale_time);
+        modelMap.addAttribute("date",date);
+        modelMap.addAttribute("city",city);
+        return "index";
+    }
+
+}
---
+++ time-zone/src/main/webapp/WEB-INF/views/success.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Microsoft Windows
+  Date: 21/03/2020
+  Time: 4:05 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Success</title>
+</head>
+<body>
+<h3>Hi ${email}</h3>
+</body>
+</html>
