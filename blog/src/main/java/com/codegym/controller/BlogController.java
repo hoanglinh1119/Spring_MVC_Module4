a0df69557b8e56c5f1621e27969b4b168d42e180
hoanglinh1119 <57972639+hoanglinh1119@users.noreply.github.com>
2020-03-30 23:30:33 +0700

    add app-blog

---
+++ blog/src/main/java/com/codegym/controller/BlogController.java
@@ -0,0 +1,65 @@
+package com.codegym.controller;
+
+import com.codegym.model.Blog;
+import com.codegym.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class BlogController {
+    @Autowired
+  private   BlogService blogService;
+    @GetMapping("/home")
+    public String getHome(Model model){
+        List<Blog> blogs=blogService.findAll();
+        model.addAttribute("blog",blogs);
+        return "home";
+    }
+    @GetMapping("/create-blog")
+    public String getCreate(Model model){
+        model.addAttribute("blog",new Blog());
+        return "create";
+    }
+    @PostMapping("/create-blog")
+    public String save(@ModelAttribute("blog") Blog blog, Model model){
+        java.util.Date date=new java.util.Date();
+        String dateDay=String.valueOf(date);
+        blog.setTime(dateDay);
+        blogService.save(blog);
+        model.addAttribute("message","them thanh cong");
+        return "/create";
+    }
+    @GetMapping("/edit-blog/{id}")
+    public String edit(@PathVariable Long id, Model model){
+        Blog blog=blogService.findById(id);
+        if (blog!=null) {
+            model.addAttribute("blog",blog);
+        }else {
+            model.addAttribute("message","sua khong thanh cong.");
+            return "edit";
+        }
+        return "edit";
+    }
+    @PostMapping("/edit-blog")
+    public String update(@ModelAttribute ("blog")Blog blog, Model model){
+        blogService.save(blog);
+        model.addAttribute(model);
+        model.addAttribute("massage","sua thanh cong");
+        return "edit";
+    }
+    @GetMapping("/view-blog/{id}")
+    public String view(@PathVariable Long id,Model model){
+        Blog blog=blogService.findById(id);
+            model.addAttribute("blog",blog);
+        return "view";
+
+    }
+
+}
---
+++ blog/src/main/java/com/codegym/model/Blog.java
@@ -0,0 +1,65 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="blog")
+public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String content;
+    private String title;
+    private String time;
+
+    public Blog(String name, String content, String title, String time) {
+        this.name=name;
+        this.content = content;
+        this.title = title;
+        this.time = time;
+    }
+
+    public Blog() {
+
+    }
+
+    public  Long getId(){
+        return id;
+    }
+    public void setId(){
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
