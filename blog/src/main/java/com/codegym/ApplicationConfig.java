830b8530be83672de471810b8815d1cc54ca8148
hoanglinh1119 <57972639+hoanglinh1119@users.noreply.github.com>
2020-04-08 21:51:01 +0700

    add

--- blog/src/main/java/com/codegym/ApplicationConfig.java
+++ blog/src/main/java/com/codegym/ApplicationConfig.java
@@ -8,9 +8,11 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -18,8 +20,12 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
@@ -29,6 +35,7 @@ import org.thymeleaf.templatemode.TemplateMode;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Locale;
 import java.util.Properties;
 
 @Configuration
@@ -36,22 +43,26 @@ import java.util.Properties;
 @EnableTransactionManagement
 @ComponentScan("com.codegym")
 public class ApplicationConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
-     private ApplicationContext applicationContext;
+    private ApplicationContext applicationContext;
+
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-      this.applicationContext=applicationContext;
+        this.applicationContext = applicationContext;
     }
+
     @Bean
-    public BlogRepository blogRepository(){
+    public BlogRepository blogRepository() {
         return new BlogRepositoryImpl();
     }
+
     @Bean
-    public BlogService blogService(){
+    public BlogService blogService() {
         return new BlogServiceImpl();
     }
+
     @Bean
-    public SpringResourceTemplateResolver templateResolver(){
-        SpringResourceTemplateResolver templateResolver=new SpringResourceTemplateResolver();
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
@@ -59,45 +70,53 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter implements Applic
         templateResolver.setTemplateMode(TemplateMode.HTML);
         return templateResolver;
     }
+
     @Bean
-    public  TemplateEngine templateEngine(){
-        TemplateEngine templateEngine=new SpringTemplateEngine();
+    public TemplateEngine templateEngine() {
+        TemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
-         return templateEngine;
+        return templateEngine;
     }
+
     @Bean
-    public ThymeleafViewResolver resolver(){
-        ThymeleafViewResolver resolver=new ThymeleafViewResolver();
-        resolver.setTemplateEngine(templateEngine());
-         resolver.setCharacterEncoding("UTF-8");
-        return resolver;
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding("UTF-8"); // <- this was added
+        viewResolver.setForceContentType(true); // <- this was added
+        viewResolver.setContentType("text/html; charset=UTF-8");
+        return viewResolver;
     }
+
     @Bean
     @Qualifier(value = "entityManager")
-    public EntityManager entityManager(EntityManagerFactory entityManagerFactory){
+    public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
         return entityManagerFactory.createEntityManager();
     }
+
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(){
-        LocalContainerEntityManagerFactoryBean em=new LocalContainerEntityManagerFactoryBean();
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource());
         em.setPackagesToScan(new String[]{"com.codegym.model"});
-        JpaVendorAdapter jpaVendorAdapter=new HibernateJpaVendorAdapter();
+        JpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(jpaVendorAdapter);
         em.setJpaProperties(additionalProperties());
-      return em;
+        return em;
     }
-    @Bean  public DataSource dataSource(){
+
+    @Bean
+    public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/blog");
-        dataSource.setUsername( "root" );
-        dataSource.setPassword( "hoanglinh" );
+        dataSource.setUrl("jdbc:mysql://localhost:3306/blog?characterEncoding=UTF-8");
+        dataSource.setUsername("root");
+        dataSource.setPassword("hoanglinh");
         return dataSource;
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(emf);
         return transactionManager;
@@ -108,6 +127,28 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter implements Applic
         properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         return properties;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        registry.addInterceptor(interceptor);
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("en"));
+        return localeResolver;
     }
 
 }
